abb6547649834d99ad90de36dae51661a5b1621a
Roland König <roland.koenig@rolandk.de>
2022-04-04 06:04:22 +0200

    Started implementing PropertyGrid on base of the implementation of MessageCommunicator

---
+++ src/FirLib.Avalonia.PropertyGridControl/_Misc.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirLib.Avalonia.PropertyGridControl
+{
+    public enum PropertyValueType
+    {
+        Unsupported,
+
+        Bool,
+
+        String,
+
+        Enum,
+
+        EncodingWebName,
+
+        TextAndHexadecimalEdit,
+         
+        DetailSettings,
+
+        FixedPossibleValues
+    }
+
+    public interface IPropertyContractResolver
+    {
+        T? GetDataAnnotation<T>(Type targetType, string propertyName)
+            where T : Attribute;
+
+        IEnumerable<Attribute> GetDataAnnotations(Type targetType, string propertyName);
+    }
+
+    public class DetailSettingsAttribute : Attribute
+    {
+        public DetailSettingsAttribute()
+        {
+
+        }
+    }
+}
